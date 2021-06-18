6c918ad56ddc5e21bb14dd11dd0318cf59ac7bc9
sshahram <shirin.shahram23@gmail.com>
2021-06-18 14:16:25 -0700

    create a ThoughtForm component

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -3,6 +3,7 @@ import { Redirect, useParams } from 'react-router-dom';
 
 import ThoughtList from '../components/ThoughtList'
 import FriendList from '../components/FriendList';
+import ThoughtForm from '../components/ThoughtForm';
 
 import { useQuery, useMutation } from '@apollo/client';
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
@@ -73,6 +74,7 @@ const Profile = props => {
           />
         </div>
       </div>
+      <div className="mb-3">{!userParam && <ThoughtForm />}</div>
     </div>
   );
 };
