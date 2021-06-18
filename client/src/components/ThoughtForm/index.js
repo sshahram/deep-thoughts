449dbb540fee58940071e750ba1d9118df7a5f64
sshahram <shirin.shahram23@gmail.com>
2021-06-18 15:30:40 -0700

    implement the add thought mutation

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,8 +1,38 @@
 import React, { useState } from 'react';
 
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
 const ThoughtForm = () => {
     const [thoughtText, setText] = useState('');
     const [characterCount, setCharacterCount] = useState(0);
+
+    // update useMutation to update the cache as well
+    const [addThought, { error }] = useMutation(ADD_THOUGHT, {
+        update(cache, { data: { addThought } }) {
+            try {
+                // could potentially not exist yet , so wrap in a try...catch
+                // read what's currently in the cache
+                const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS });
+
+                // prepend the newest thought to the front of the array
+                cache.writeQuery({
+                    query: QUERY_THOUGHTS,
+                    data: { thoughts: [addThought, ...thoughts] }
+                });
+            } catch (e) {
+                console.error(e);
+            }
+
+            // update me object's cache, appending new thought to the end of the array
+            const { me } = cache.readQuery({ query: QUERY_ME });
+            cache.writeQuery({
+                query: QUERY_ME,
+                data: { me : { ...me, thoughts: [...me.thoughts, addThought] } }
+            });
+        }
+    });
 
     // function for when typing (changing) text in the form
     const handleChange = event => {
@@ -13,20 +43,32 @@ const ThoughtForm = () => {
     };
 
     // function to handle submitting the form
-    const handleSubmit = async event => {
+    const handleFormSubmit = async event => {
         event.preventDefault();
-        setText('');
-        setCharacterCount(0);
+
+        try {
+            // add thought to database
+            await addThought({
+                variables: { thoughtText }
+            });
+
+            // clear form value
+            setText('');
+            setCharacterCount(0);
+        } catch (e) {
+            console.error(e);
+        }
     };
 
     return (
         <div>
-            <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className="ml-2">Something went wrong...</span>}
             </p>
             <form
                 className="flex-row justify-center justify-space-between-md align-stretch"
-                onSubmit={handleSubmit}
+                onSubmit={handleFormSubmit}
             >
                 <textarea
                     placeholder="Here's a new thought..."
