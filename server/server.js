2dde6d466ea40d5d95c901ea6cc6b395e1e0de93
sshahram <shirin.shahram23@gmail.com>
2021-06-19 00:24:26 -0700

    change serviceWorker path in server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ if(process.env.NODE_ENV === 'production') {
 }
 
 app.get("/service-worker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "public", "service-worker.js"));
+  res.sendFile(path.resolve(__dirname, "public", "serviceWorker.js"));
 });
 
 app.get('*', (req, res) => {
