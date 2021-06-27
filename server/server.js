54888a952e669dffe574106c75b48ecf3d434578
sshahram <shirin.shahram23@gmail.com>
2021-06-27 13:03:22 -0700

    remove serviceWorker.js

--- server/server.js
+++ server/server.js
@@ -29,10 +29,6 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.get("/serviceWorker.js", (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/public/serviceWorker.js'));
-});
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
